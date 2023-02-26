fee2191e15cb939265f5455e590f4c60c3c992fa
Che Hongle <chehongle123@163.com>
2023-02-26 00:02:08 -0600

    finish parsing and writing

--- include/http_conn.h
+++ include/http_conn.h
@@ -20,6 +20,7 @@
 #include <cerrno>
 #include "locker.h"
 #include <sys/uio.h>
+#include <cstring>
 
 // Http 任务类
 class HttpConn {
@@ -30,7 +31,52 @@ public:
 
     static const size_t READ_BUFFER_SIZE = 2048; // 读缓冲大小
 
-    static const size_t WRITE_BUFFER_SIZE = 1024;
+    static const size_t WRITE_BUFFER_SIZE = 1024; // 写缓冲大小
+
+    static const int FILENAME_LEN = 200;        // 文件名的最大长度
+
+    // HTTP请求方法，这里只支持GET
+    enum METHOD {
+        GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT
+    };
+
+    /*
+        解析客户端请求时，主状态机的状态
+        CHECK_STATE_REQUESTLINE:当前正在分析请求行
+        CHECK_STATE_HEADER:当前正在分析头部字段
+        CHECK_STATE_CONTENT:当前正在解析请求体
+    */
+    enum CHECK_STATE {
+        CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT
+    };
+
+    /*
+        服务器处理HTTP请求的可能结果，报文解析的结果
+        NO_REQUEST          :   请求不完整，需要继续读取客户数据
+        GET_REQUEST         :   表示获得了一个完成的客户请求
+        BAD_REQUEST         :   表示客户请求语法错误
+        NO_RESOURCE         :   表示服务器没有资源
+        FORBIDDEN_REQUEST   :   表示客户对资源没有足够的访问权限
+        FILE_REQUEST        :   文件请求,获取文件成功
+        INTERNAL_ERROR      :   表示服务器内部错误
+        CLOSED_CONNECTION   :   表示客户端已经关闭连接了
+    */
+    enum HTTP_CODE {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+
+    // 从状态机的三种可能状态，即行的读取状态，分别表示
+    // 1.读取到一个完整的行 2.行出错 3.行数据尚且不完整
+    enum LINE_STATUS {
+        LINE_OK = 0, LINE_BAD, LINE_OPEN
+    };
 
     HttpConn();
 
@@ -52,9 +98,64 @@ private:
     struct sockaddr_in m_address{}; // client地址
 
     char m_read_buf[READ_BUFFER_SIZE]; // 读缓存
+    int m_read_idx; // 记录当前读缓冲器最后一个字符的下一个位置
+    int m_checked_idx; // 当前在parse的缓冲区的位置
+    int m_start_line; // 行数据起始位置
 
-    int m_read_idx; // 记录读取位置
+    char *m_url; // 请求目标文件名
+    char m_real_file[FILENAME_LEN]; // 请求的目标文件的完整路径，等于 doc_root + m_url, doc_root是网站根目录
+    char *m_version; // 协议版本
+    char *m_host; // 主机名
+    bool m_linger; // 是否要保持连接
+    METHOD m_method; // 请求方法
+    int m_content_length;
+    CHECK_STATE m_check_state; // 当前主机状态
 
+    char m_write_buf[WRITE_BUFFER_SIZE]; // 写缓冲区
+    int m_write_idx; // 写缓冲区中待发送的字节数
+    char *m_file_address; // 请求的目标文件被mmap到内存中的起始位置
+    struct stat m_file_stat; // 目标文件的状态
+    struct iovec m_iv[2]; // 采用writev来执行写操作，定义下面两个成员，其中m_iv_count表示被写内存块的数量
+    int m_iv_count;
+
+    int bytes_to_send; // 将要发送的数据的字节数
+    int bytes_have_send; // 已经发送的字节数
+
+    void Init(); // 初始化请求
+
+    LINE_STATUS ParseLine();
+
+    HTTP_CODE ProcessRead();
+
+    bool ProcessWrite(HTTP_CODE read_ret);
+
+    HTTP_CODE ParseRequest(char *text);
+
+    HTTP_CODE ParseHeader(char *text);
+
+    HTTP_CODE ParseContent(char *text);
+
+    HTTP_CODE DoRequest();
+
+    char *GetLine();
+
+    void Unmap();
+
+    bool AddResponse(const char *format, ...);
+
+    bool AddContent(const char *content);
+
+    bool AddContentType();
+
+    bool AddStatusLine(int status, const char *title);
+
+    void AddHeaders(int content_length);
+
+    bool AddContentLength(int content_length);
+
+    bool AddLinger();
+
+    bool AddBlankLine();
 };
 
 #endif //WEBSERVER_HTTP_CONN_H
--- src/http_conn.cpp
+++ src/http_conn.cpp
@@ -5,10 +5,20 @@
 #include "http_conn.h"
 
 int HttpConn::m_user_num = 0;
-
 int HttpConn::m_epoll_fd = -1;
 
+static const char *doc_root = "/home/index/WebServer/resources"; // 网站根目录
+static const char *ok_200_title = "OK";
+static const char *error_400_title = "Bad Request";
+static const char *error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+static const char *error_403_title = "Forbidden";
+static const char *error_403_form = "You do not have permission to get file from this server.\n";
+static const char *error_404_title = "Not Found";
+static const char *error_404_form = "The requested file was not found on this server.\n";
+static const char *error_500_title = "Internal Error";
+static const char *error_500_form = "There was an unusual problem serving the requested file.\n";
 
+// epoll相关函数============================================================
 void AddFd(int epoll_fd, int fd, bool one_shot) {
     struct epoll_event event{};
     event.data.fd = fd;
@@ -32,26 +42,336 @@ void RemoveFd(int epoll_fd, int fd) {
 void ModFd(int epoll_fd, int fd, int ev) {
     struct epoll_event event{};
     event.data.fd = fd;
-    event.events = ev | EPOLLONESHOT | EPOLLRDHUP; // 重置ONE_SHOT事件
+    event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP; // 重置ONE_SHOT事件
     epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &event);
 }
+//==========================================================================
 
+HttpConn::HttpConn() {}
 
-HttpConn::HttpConn() {
+HttpConn::~HttpConn() {}
 
+void HttpConn::Init() {
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_checked_idx = 0;
+    m_start_line = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    m_url = nullptr;
+    m_method = GET;
+    m_version = nullptr;
+    m_content_length = 0;
+    m_host = nullptr;
+    m_linger = false;
+
+    memset(m_read_buf, 0, READ_BUFFER_SIZE);
+    memset(m_write_buf, 0, READ_BUFFER_SIZE);
+    memset(m_real_file, 0, FILENAME_LEN);
 }
 
-HttpConn::~HttpConn() {
+HttpConn::LINE_STATUS HttpConn::ParseLine() {
+    // 根据/r/n判断一行数据，然后解析，具体方法为替换成字符串结束符\0
+    char temp;
+    for (; m_checked_idx < m_read_idx; m_checked_idx++) {
+        temp = m_read_buf[m_checked_idx];
+        if (temp == '\r') {
+            if ((m_checked_idx + 1) == m_read_idx) { // 读到了末尾，但是没有换行符号
+                return LINE_OPEN;
+            } else if (m_read_buf[m_checked_idx] + 1 == '\n') {
+                m_read_buf[m_checked_idx++] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        } else if (temp == '\n') {
+            if ((m_checked_idx > 1) && (m_read_buf[m_checked_idx - 1] == '\r')) {
+                m_read_buf[m_checked_idx - 1] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_OK;
+}
 
+HttpConn::HTTP_CODE HttpConn::ParseRequest(char *text) {
+    m_url = strpbrk(text, "\t");
+    if (!m_url) {
+        return BAD_REQUEST;
+    }
+    *m_url++ = '\0';
+    char *method = text;
+    if (strcasecmp(method, "GET") == 0) {
+        m_method = GET;
+    } else {
+        printf("Only support GET method\n");
+        return BAD_REQUEST;
+    }
+
+    m_version = strpbrk(m_url, "\t");
+    if (!m_version) {
+        return BAD_REQUEST;
+    }
+    *m_version++ = '\0';
+    if (strcasecmp(m_version, "HTTP/1.1") != 0) {
+        return BAD_REQUEST;
+    }
+
+    if (strncasecmp(m_url, "http://", 7) == 0) {
+        m_url += 7;
+        m_url = strchr(m_url, '/');
+    }
+
+    if (!m_url || m_url[0] != '/') {
+        return BAD_REQUEST;
+    }
+
+    m_check_state = CHECK_STATE_HEADER; // 状态转移
+    return NO_REQUEST;
+}
+
+HttpConn::HTTP_CODE HttpConn::ParseHeader(char *text) {
+    // 遇到空行，表示头部字段解析完毕
+    if (text[0] == '\0') {
+        // 如果HTTP请求有消息体，则还需要读取m_content_length字节的消息体，
+        // 状态机转移到CHECK_STATE_CONTENT状态
+        if (m_content_length != 0) {
+            m_check_state = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        return GET_REQUEST;
+    } else if (strncasecmp(text, "Connection:", 11) == 0) {
+        // 处理Connection 头部字段  Connection: keep-alive
+        text += 11;
+        text += strspn(text, " \t");
+        if (strcasecmp(text, "keep-alive") == 0) {
+            m_linger = true;
+        }
+    } else if (strncasecmp(text, "Content-Length:", 15) == 0) {
+        // 处理Content-Length头部字段
+        text += 15;
+        text += strspn(text, " \t");
+        m_content_length = atol(text);
+    } else if (strncasecmp(text, "Host:", 5) == 0) {
+        // 处理Host头部字段
+        text += 5;
+        text += strspn(text, " \t");
+        m_host = text;
+    } else {
+        printf("oop! unknow header %s\n", text);
+    }
+    return NO_REQUEST;
+}
+
+HttpConn::HTTP_CODE HttpConn::ParseContent(char *text) {
+    // 没有真正实现，只判断是否读入
+    if (m_read_idx >= (m_content_length + m_checked_idx)) {
+        text[m_content_length] = '\0';
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
+}
+
+HttpConn::HTTP_CODE HttpConn::DoRequest() {
+    // 寻找资源，写给client
+    strcpy(m_real_file, doc_root);
+    int len = strlen(doc_root);
+    strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+    // 获取m_real_file文件的相关的状态信息，-1失败，0成功
+    if (stat(m_real_file, &m_file_stat) < 0) {
+        return NO_RESOURCE;
+    }
+
+    // 判断访问权限
+    if (!(m_file_stat.st_mode & S_IROTH)) {
+        return FORBIDDEN_REQUEST;
+    }
+
+    // 判断是否是目录
+    if (S_ISDIR(m_file_stat.st_mode)) {
+        return BAD_REQUEST;
+    }
+
+    // 以只读方式打开文件
+    int fd = open(m_real_file, O_RDONLY);
+    // 创建内存映射
+    m_file_address = (char *) mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
+}
+
+char *HttpConn::GetLine() {
+    return m_read_buf + m_start_line; // 跳转到开始行
+}
+
+void HttpConn::Unmap() {
+    if (m_file_address) {
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
+bool HttpConn::AddResponse(const char *format, ...) {
+    if (m_write_idx >= WRITE_BUFFER_SIZE) {
+        return false;
+    }
+    va_list arg_list;
+    va_start(arg_list, format);
+    int len = vsnprintf(m_write_buf + m_write_idx, WRITE_BUFFER_SIZE - 1 - m_write_idx, format, arg_list);
+    if (len >= (WRITE_BUFFER_SIZE - 1 - m_write_idx)) {
+        return false;
+    }
+    m_write_idx += len;
+    va_end(arg_list);
+    return true;
+}
+
+bool HttpConn::AddStatusLine(int status, const char *title) {
+    return AddResponse("%s %d %s\r\n", "HTTP/1.1", status, title);
+}
+
+void HttpConn::AddHeaders(int content_len) {
+    AddContentLength(content_len);
+    AddContentType();
+    AddLinger();
+    AddBlankLine();
+}
+
+bool HttpConn::AddContentLength(int content_len) {
+    return AddResponse("Content-Length: %d\r\n", content_len);
+}
+
+bool HttpConn::AddLinger() {
+    return AddResponse("Connection: %s\r\n", (m_linger == true) ? "keep-alive" : "close");
+}
+
+bool HttpConn::AddBlankLine() {
+    return AddResponse("%s", "\r\n");
+}
+
+bool HttpConn::AddContent(const char *content) {
+    return AddResponse("%s", content);
+}
+
+bool HttpConn::AddContentType() {
+    return AddResponse("Content-Type:%s\r\n", "text/html");
+}
+
+HttpConn::HTTP_CODE HttpConn::ProcessRead() {
+    LINE_STATUS line_status = LINE_OK;
+    HTTP_CODE ret = NO_REQUEST;
+
+    char *text = nullptr;
+
+    while ((m_check_state == CHECK_STATE_CONTENT) && (line_status == LINE_OK) ||
+           (line_status = ParseLine()) == LINE_OK) { // 收到一行数据，准备处理，数据保存在
+        text = GetLine();
+        printf("Parsing one line:\n%s\n", text);
+        m_start_line = m_checked_idx;
+
+        switch (m_check_state) {
+            case CHECK_STATE_REQUESTLINE:
+                ret = ParseRequest(text);
+                if (ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                }
+                break;
+            case CHECK_STATE_HEADER:
+                ret = ParseHeader(text);
+                if (ret == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                } else if (ret == GET_REQUEST) {
+                    return DoRequest();
+                }
+                break;
+            case CHECK_STATE_CONTENT:
+                ret = ParseContent(text);
+                if (ret == GET_REQUEST) {
+                    return DoRequest();
+                }
+                line_status = LINE_OPEN;
+                break;
+            default:
+                return INTERNAL_ERROR;
+        }
+
+        return NO_REQUEST;
+    }
+}
+
+bool HttpConn::ProcessWrite(HttpConn::HTTP_CODE ret) {
+    switch (ret) {
+        case INTERNAL_ERROR:
+            AddStatusLine(500, error_500_title);
+            AddHeaders(strlen(error_500_form));
+            if (!AddContent(error_500_form)) {
+                return false;
+            }
+            break;
+        case BAD_REQUEST:
+            AddStatusLine(400, error_400_title);
+            AddHeaders(strlen(error_400_form));
+            if (!AddContent(error_400_form)) {
+                return false;
+            }
+            break;
+        case NO_RESOURCE:
+            AddStatusLine(404, error_404_title);
+            AddHeaders(strlen(error_404_form));
+            if (!AddContent(error_404_form)) {
+                return false;
+            }
+            break;
+        case FORBIDDEN_REQUEST:
+            AddStatusLine(403, error_403_title);
+            AddHeaders(strlen(error_403_form));
+            if (!AddContent(error_403_form)) {
+                return false;
+            }
+            break;
+        case FILE_REQUEST:
+            AddStatusLine(200, ok_200_title);
+            AddHeaders(m_file_stat.st_size);
+            m_iv[0].iov_base = m_write_buf;
+            m_iv[0].iov_len = m_write_idx;
+            m_iv[1].iov_base = m_file_address;
+            m_iv[1].iov_len = m_file_stat.st_size;
+            m_iv_count = 2;
+
+            bytes_to_send = m_write_idx + m_file_stat.st_size;
+
+            return true;
+        default:
+            return false;
+    }
+
+    m_iv[0].iov_base = m_write_buf;
+    m_iv[0].iov_len = m_write_idx;
+    m_iv_count = 1;
+    bytes_to_send = m_write_idx;
+    return true;
 }
 
 void HttpConn::Process() {
     // 解析Http请求
     printf("A thread is parsing http request\n");
+    HTTP_CODE read_ret = ProcessRead();
+    if (read_ret == NO_REQUEST) {
+        ModFd(m_epoll_fd, m_conn_fd, EPOLLIN);
+        return;
+    }
     // 生成响应
-
+    printf("A thread is generating http response\n");
+    bool write_ret = ProcessWrite(read_ret);
+    if (!write_ret) {
+        CloseConn();
+    }
     // 重新注册fd，方便下一个线程进行处理
-    ModFd(m_epoll_fd, m_conn_fd, EPOLLIN);
+    ModFd(m_epoll_fd, m_conn_fd, EPOLLOUT);
 }
 
 void HttpConn::AddConn(int conn_fd, const sockaddr_in &client_address) {
@@ -61,6 +381,8 @@ void HttpConn::AddConn(int conn_fd, const sockaddr_in &client_address) {
     // 添加epoll对象进行监听
     AddFd(m_epoll_fd, conn_fd, true);
     m_user_num++;
+
+    Init();
 }
 
 void HttpConn::CloseConn() {
@@ -90,11 +412,58 @@ bool HttpConn::Read() {
         m_read_idx += bytes_read;
     }
 
-    printf("Read data from %d, data:\n %s\n", m_address.sin_addr.s_addr, m_read_buf);
+    printf("Read data:\n %s\n", m_read_buf);
     return true;
 }
 
 bool HttpConn::Write() {
-    printf("Write all data in one time\n");
-    return true;
+    int temp = 0;
+
+    if (bytes_to_send == 0) {
+        // 将要发送的字节为0，这一次响应结束。
+        ModFd(m_epoll_fd, m_conn_fd, EPOLLIN);
+        Init();
+        return true;
+    }
+
+    while (true) {
+        // 分散写
+        temp = writev(m_conn_fd, m_iv, m_iv_count);
+        if (temp <= -1) {
+            // 如果TCP写缓冲没有空间，则等待下一轮EPOLLOUT事件，虽然在此期间，
+            // 服务器无法立即接收到同一客户的下一个请求，但可以保证连接的完整性。
+            if (errno == EAGAIN) {
+                ModFd(m_epoll_fd, m_conn_fd, EPOLLOUT);
+                return true;
+            }
+            Unmap();
+            return false;
+        }
+
+        bytes_have_send += temp;
+        bytes_to_send -= temp;
+
+        if (bytes_have_send >= m_iv[0].iov_len) {
+            m_iv[0].iov_len = 0;
+            m_iv[1].iov_base = m_file_address + (bytes_have_send - m_write_idx);
+            m_iv[1].iov_len = bytes_to_send;
+        } else {
+            m_iv[0].iov_base = m_write_buf + bytes_have_send;
+            m_iv[0].iov_len = m_iv[0].iov_len - temp;
+        }
+
+        if (bytes_to_send <= 0) {
+            // 没有数据要发送了
+            Unmap();
+            ModFd(m_epoll_fd, m_conn_fd, EPOLLIN);
+
+            if (m_linger) {
+                Init();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
 }
